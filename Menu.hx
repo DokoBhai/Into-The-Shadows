f1001eae164eb79ad82af62af4b08d9d81fee6a6
DokoBhai <159497103+DokoBhai@users.noreply.github.com>
2025-06-17 20:19:53 -0700

    Menu state

---
+++ Menu.hx
@@ -0,0 +1,13 @@
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxState;
+import flixel.graphics.FlxGraphic;
+import flixel.group.FlxGroup;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import openfl.text.TextFieldAutoSize;
+
+class Menu extends FlxState{
+
+}
